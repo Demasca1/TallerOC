44c5766b01a3775bd7325cfe481b50ea49442ffd
Demasca1 <alumno29.demascarelli.santino@ipm.edu.ar>
2026-06-23 09:26:38 -0300

    Juego impostor
    Hicimos un juego del impostor

---
+++ misproyectos/Phyton/Dia4/ejimpostor.py
@@ -0,0 +1,23 @@
+import os
+import time
+import random
+num=1
+jugadores=[]
+frutas=["Manzana", "Banana", "Naranja", "Frutilla"] 
+jug=input("¿Cuantos jugadores son?: ")
+cantidad_jugador=int(jug)
+for i in range (cantidad_jugador):
+    Nombre=input(f"Introduce el nombre del jugador {num}: ")
+    jugadores.append(Nombre)
+    num=num+1
+impostor=random.choice(jugadores)
+palabra=random.choice(frutas)
+for i in range (cantidad_jugador):
+    if impostor==jugadores[i]:
+        print(f"Turno de {jugadores[i]}:Eres el impostor")
+        time.sleep(4)
+        print("\033[F\033[K", end="")
+    else:
+        print(f"Turno de {jugadores[i]}: Eres inocente, la palabra es: {palabra}")
+        time.sleep(4)
+        print("\033[F\033[K", end="")  
